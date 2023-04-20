3ae3cb67df124ff1f0101fe7ceffcd65276fc1bf
KovalenkoAE <sasha@kovalenko.net.ru>
2023-04-20 10:00:57 +0300

    Project structure creation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Oscillogram analysis by ML/DL algorithms',
+    author='Oscillogram team',
+    license='MIT',
+)
