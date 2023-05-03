aaf2e4eb21982e3c366c7e672982df7bb4fedf08
KovalenkoAE <sasha@kovalenko.net.ru>
2023-05-03 16:46:01 +0300

    readme correction

--- src/models/train.py
+++ src/models/train.py
@@ -1,15 +1,11 @@
-import os
-os.chdir('../..')
-
 import pandas as pd
 import torch
 import torch.nn as nn
 from torch.optim import Adam
+from sklearn.metrics import f1_score
 
-print(os.getcwdb())
-
-from src.data.dataloader import OscillogramDataLoader
 from src.models.mlp import MLP
+from src.data.dataloader import OscillogramDataLoader
 
 
 def train():
@@ -33,7 +29,6 @@ def train():
         step_size=1,
         minibatch_training=True,
         batch_size=256,
-        shuffle=True
     )
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -43,8 +38,9 @@ def train():
     n_epochs = 10
     loss_func = nn.BCELoss()
     
+    print('\n Training:')
+    model.train()
     for e in range(n_epochs):
-        model.train()
         av_loss = []
         for train_ts, train_index, train_label in train_dl:
             x = torch.FloatTensor(train_ts)
@@ -57,6 +53,22 @@ def train():
             optimizer.step()
             av_loss.append(loss.item())
         print(f'Epoch: {e+1:2d}/{n_epochs}, average CE loss: {sum(av_loss)/len(av_loss):.4f}')
+
+    print('\n Testing:')
+    model.eval()
+    preds = []
+    test_labels = []
+    for test_ts, test_index, test_label in test_dl:
+        x = torch.FloatTensor(test_ts)
+        with torch.no_grad():
+            logits = model(x)
+        p = torch.round(torch.squeeze(logits))
+        preds.append(pd.Series(p, index=test_index))
+        test_labels.append(test_label)
+    pred = pd.concat(preds)
+    test_label = pd.concat(test_labels)
+    score = f1_score(test_label, pred)
+    print('f1_score: ', score)
         
 
 if __name__ == "__main__":
